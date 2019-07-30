a4f8544a0879effe28c17561bce43ff0c4f0f330
edwd42 <edwd42@yahoo.com>
2019-07-29 20:52:01 -0400

    change JRE build path and compliance level to 1.8

---
+++ src/main/java/net/ed/utils/ILoggable.java
@@ -0,0 +1,10 @@
+package net.ed.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public interface ILoggable {
+	
+	final Logger logger = LoggerFactory.getLogger(ILoggable.class);
+
+}

4dabf1b4ca1e83658d1756d5629f16f12dc86059
edwd42 <edwd42@yahoo.com>
2019-08-01 14:07:33 -0400

    add Scanner to read login credentials

--- src/main/java/net/ed/yahoo_scraper/App.java
+++ src/main/java/net/ed/yahoo_scraper/App.java
@@ -1,15 +1,15 @@
 package net.ed.yahoo_scraper;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class App 
 {
-    public static void main( String[] args )
-    {
 
+	public static void main( String[] args )
+    {
     	LoginController login = new LoginController();
     	login.loadWebDriver();
-    	login.unloadWebDriver();
+    	login.navigate();
+//    	login.unloadWebDriver();
     }
+    
+
 }
--- src/main/java/net/ed/yahoo_scraper/LoginController.java
+++ src/main/java/net/ed/yahoo_scraper/LoginController.java
@@ -2,14 +2,24 @@ package net.ed.yahoo_scraper;
 
 import net.ed.utils.ILoggable;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginController implements ILoggable {
 	
+    private static String[] loginCredentials = new String[4];
+
 	private WebDriver driver;
 	
-	public String loadWebDriver() {
+	public WebDriver loadWebDriver() {
 		
 		// add support for chrome driver
 		String keyC = "webdriver.chrome.driver";
@@ -17,20 +27,70 @@ public class LoginController implements ILoggable {
 		System.setProperty(keyC, valueC);
 		
 		driver = new ChromeDriver(); // launch chrome
+		return driver;
+	}
+	
+	public void navigate() {
 		
-		String url = "https://login.yahoo.com";
-		driver.get(url);
+		driver.get("https://login.yahoo.com");
 		
 		String pageTitle = driver.getTitle();
 		logger.debug(pageTitle);
-		return pageTitle;
 		
+		getLoggedIn();
 	}
 	
 	public void unloadWebDriver() {
 	    if (driver != null) {
+	    	logger.debug("unloading driver");
 	    	driver.quit();
 	    }
 	}
 
+    public static String[] readloginCredentials() {
+    	
+    	String filename = "src/main/resources/yahoo.txt";
+        
+		try {
+			File file = new File(filename);
+			Scanner scnr = new Scanner(file);
+	        int lineNumber = 0;
+	        while(scnr.hasNextLine()){
+	            String line = scnr.nextLine();
+	            loginCredentials[lineNumber] = line;
+	            lineNumber++;
+	        }
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		return loginCredentials; 
+    }
+
+	public WebDriver getLoggedIn() {
+		
+		readloginCredentials();
+		
+		String username = loginCredentials[1];
+		String password = loginCredentials[2];
+		
+		try {
+			
+			driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);			 
+			driver.findElement(By.name("username")).sendKeys(username + Keys.ENTER); // fill in the blanks
+			driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+//			driver.findElement(By.name("signin")).click();
+			driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+			driver.findElement(By.name("password")).sendKeys(password + Keys.ENTER);
+			driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+//			driver.findElement(By.name("verifyPassword")).click();
+			System.out.println("46. fin getLoggedIn()... " + driver.getCurrentUrl());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			driver.quit();
+		}
+		return driver;
+	}
+	
 }
